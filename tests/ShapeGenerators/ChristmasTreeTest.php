b2bda1e497bdeba687eecc455eb3fb59ac3d6b2a
strlcp <strlcp@gmx.de>
2018-04-05 06:58:42 +0200

    introducing dataproviders

--- tests/ShapeGenerators/ChristmasTreeTest.php
+++ tests/ShapeGenerators/ChristmasTreeTest.php
@@ -31,36 +31,70 @@ class ChristmasTreeTest extends PHPUnit_Framework_TestCase {
 /**
 * 	@covers \monaid\AsciiArt\ShapeGenerators\ChristmasTree::calculateLength
 *	@depends testConstructor
+*	@dataProvider christmasTreeSizesProvider
 */ 
-	public function testGetHeight()  {
+	public function testGetHeight($x, $y)  {
 	  $method = self::$reflection->getMethod('getHeight');
 	  $method->setAccessible(true);
-	  $this->assertEquals($method->invoke(self::$christmasTree, 7), 5);
+	  $this->assertEquals($method->invoke(self::$christmasTree, $x), $y);
 	
 	}
       
 /**
 * 	@covers \monaid\AsciiArt\ShapeGenerators\ChristmasTree::calculateLength
 *	@depends testConstructor
+*	@dataProvider ChristmasTreePatternLengthProvider
 */  
       
-      public function testCalculateLength() {
+      public function testCalculateLength($x, $y) {
 	  $method = self::$reflection->getMethod('calculateLength');
 	  $method->setAccessible(true);
-	  $this->assertEquals($method->invoke(self::$christmasTree, 2), 1);
+	  $this->assertEquals($method->invoke(self::$christmasTree, $y), $x);
 	 
        }
 /**
 * 	@covers \monaid\AsciiArt\ShapeGenerators\ChristmasTree::getWidth	
 *	@depends testCalculateLength
+*	@dataProvider christmasTreeSizesProvider
 */
 
-      public function testGetWidth(){
+      public function testGetWidth($x, $y){
 	    $method = self::$reflection->getMethod('getWidth');
 	    $method->setAccessible(true);
-	    $this->assertEquals($method->invoke(self::$christmasTree, 3), 3);
+	    $this->assertEquals($method->invoke(self::$christmasTree, $y), $x);
       }
 
-
-
-}
+/**
+*	Dataproviders
+*/
+      
+/**
+*	Provides data for testGetHeight and testGetWidth	
+*	
+*/
+      public function christmasTreeSizesProvider() {
+	    return [
+		[7, 5],
+		[11, 7],
+		[19, 11]
+ 	      ];
+      }
+      
+/**
+*	Provides data for calculateLength 
+*	may use same provider for the getHeight/getWidth tests 
+*	
+*/     
+       public function christmasTreePatternLengthProvider() {
+	    return [
+		[1, 2],
+		[3, 3],
+		[7, 5],
+		[9, 6],
+		[11, 7],
+		[17, 10],
+		[19, 11]
+ 	      ];
+      }
+      
+}      
